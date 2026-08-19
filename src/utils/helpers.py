a37ac44e93d0eaae949c203560cd0b4b9e628f10
Andres Vasquez <andres.dominios27@gmail.com>
2026-08-18 22:00:27 -0600

    feat:add ticket history on status change

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -2,6 +2,7 @@ from fastapi import Depends, HTTPException
 from sqlalchemy.orm import Session
 from src.models.ticket import Ticket
 from src.models.user import User
+from src.models.ticket_history import TicketHistory
 
 #-----------SearchTickets--------------------
 def get_ticket_or_404(ticket_id: int, db: Session)->Ticket:
@@ -26,4 +27,16 @@ def validate_active_user(user: User):
 def verify_ticket_owner(ticket: Ticket, current_user: User):
     if ticket.user_id != current_user.id:
        raise HTTPException(status_code=403, detail="Not authorized to access this ticket")
+
+#-----------RegisterTicketHistoty--------------------
+def register_ticket_history(ticket: Ticket, new_status: str, current_user: User, db: Session):
+    if ticket.status != new_status:
+        history = TicketHistory(
+            ticket_id = ticket.id,
+            changed_by= current_user.id,
+            previous_status= ticket.status,
+            new_status= new_status
+        )
+        db.add(history)
+    
     
